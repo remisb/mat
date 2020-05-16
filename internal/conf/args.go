f2e50ab424e8dae91852d06934cad7fa82e3c8ed
Remigijus Bauzys <bauzys@gmail.com>
2020-05-16 10:43:43 +0300

    removed redundant argsT type

--- internal/conf/args.go
+++ internal/conf/args.go
@@ -1,21 +1,18 @@
 package conf
 
-import "reflect"
-
 // Args holds command line arguments after flags have been parsed.
 type Args struct {
 	Args       []string
 	ConfigPath string
 }
 
+// NewConfigArgs is a factory function which create Args structure. command line args should
+// be passed as an argument.
 func NewConfigArgs(args []string) Args {
 	return Args{
 		Args: args,
 	}
 }
-
-// argsT is used by Parse and Usage to detect struct fields of the Args type.
-var argsT = reflect.TypeOf(Args{})
 
 // Num returns the i'th argument in the Args slice. It returns an empty string
 // the request element is not present.
